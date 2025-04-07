04a6ba46ec1474915572a8fdc6ab3423dd665784
NicholasRader <nicholasmrader@gmail.com>
2025-04-06 22:24:41 -0400

    Cleanup

--- recipiece-3/server/routes/announcementRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const announcementController = require('../controllers/announcementController');
-
-const router = express.Router();
-
-router.post('/add-announcement', announcementController.postAnnouncement);
-router.get('/announcements', announcementController.getAllAnnouncements);
-
-module.exports = router;
